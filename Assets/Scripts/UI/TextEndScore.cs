26979e28390a0e838baf3a1f5d1fd401de5f8c2d
LaCreArthur <le-tutur@hotmail.fr>
2025-01-08 16:34:56 +0100

    feat: improved end score text if high score

--- Assets/Scripts/UI/TextEndScore.cs
+++ Assets/Scripts/UI/TextEndScore.cs
@@ -7,5 +7,7 @@ public class TextEndScore : MonoBehaviour
     TextMeshProUGUI _text;
     void Awake() => _text = GetComponent<TextMeshProUGUI>();
     void OnEnable() => UpdateScoreText();
-    void UpdateScoreText() => _text.text = $"score: {ScoreManager.Score}\nhigh score: {ScoreManager.HighScore}";
+    void UpdateScoreText() => _text.text =
+        ScoreManager.Score == ScoreManager.HighScore ? $"New high score: {ScoreManager.HighScore}!" :
+            $"score: {ScoreManager.Score} - High score: {ScoreManager.HighScore}";
 }
