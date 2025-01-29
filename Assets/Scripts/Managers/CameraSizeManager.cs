4431cb9c082bfde6680d5fb48b2279718f5b104e
LaCreArthur <le-tutur@hotmail.fr>
2025-01-29 18:07:03 +0100

    feat: switch camera size on play mode

---
+++ Assets/Scripts/Managers/CameraSizeManager.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Cinemachine;
+using UnityEngine;
+
+[RequireComponent(typeof(CinemachineCamera))]
+public class CameraSizeManager : MonoBehaviour
+{
+    [SerializeField] float homeSize = 3;
+    [SerializeField] float gameSize = 7;
+    [SerializeField] float lerpSpeed = 5;
+
+    CinemachineCamera _camera;
+
+    float _currentSize;
+    float _targetSize;
+    bool _isLerp;
+
+    void Start()
+    {
+        _camera = GetComponent<CinemachineCamera>();
+        _currentSize = _camera.Lens.OrthographicSize;
+        GameStateManager.OnHome += SetHomeSize;
+        GameStateManager.OnPlaying += SetGameSize;
+    }
+
+    void Update()
+    {
+        if (!_isLerp) return;
+        _currentSize = Mathf.Lerp(_currentSize, _targetSize, lerpSpeed * Time.deltaTime);
+        _camera.Lens.OrthographicSize = _currentSize;
+        if (Mathf.Abs(_currentSize - _targetSize) < 0.01f)
+        {
+            _isLerp = false;
+            _camera.Lens.OrthographicSize = _targetSize;
+        }
+    }
+
+    void OnDestroy()
+    {
+        GameStateManager.OnHome -= SetHomeSize;
+        GameStateManager.OnPlaying -= SetGameSize;
+    }
+
+    void SetHomeSize()
+    {
+        _targetSize = homeSize;
+        _isLerp = true;
+    }
+
+    void SetGameSize()
+    {
+        _targetSize = gameSize;
+        _isLerp = true;
+    }
+}
