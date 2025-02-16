f0fba0c44feacd8538cd36905366b3c65d243f6b
LaCreArthur <le-tutur@hotmail.fr>
2025-02-16 13:49:39 +0100

    fix: fixed SpriteLookTowardInput switching at no input

--- Assets/Scripts/Sprites/SpriteLookTowardInput.cs
+++ Assets/Scripts/Sprites/SpriteLookTowardInput.cs
@@ -3,5 +3,9 @@
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteLookTowardInput : SpriteLookTo
 {
-    protected override void SetLookingLeft() => lookingLeft = InputManager.Input.x < InputManager.DEAD_ZONE;
+    protected override void SetLookingLeft()
+    {
+        if (Mathf.Approximately(0, InputManager.Input.x)) return;
+        lookingLeft = InputManager.Input.x < -InputManager.DEAD_ZONE;
+    }
 }
